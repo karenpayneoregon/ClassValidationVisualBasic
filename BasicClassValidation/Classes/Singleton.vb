ce1bc1871173dab53d2745523fdaca184744721b
Karen Payne <paynekaren@comcast.net>
2019-06-21 03:22:33 -0700

    Added a singleton class

---
+++ BasicClassValidation/Classes/Singleton.vb
@@ -0,0 +1,12 @@
+﻿Public NotInheritable Class ApplicationData
+    Private Sub New()
+    End Sub
+
+    Public Property AccountNumber() As String
+    Private Shared ReadOnly Lazy As New Lazy(Of ApplicationData)(Function() New ApplicationData())
+    Public Shared ReadOnly Property Instance() As ApplicationData
+        Get
+            Return Lazy.Value
+        End Get
+    End Property
+End Class
