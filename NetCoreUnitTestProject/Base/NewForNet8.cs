8e674e407c609aa7ca1a5e7bf4400cef57eeacb4
Karen Payne <paynekaren@comcast.net>
2023-12-04 08:20:17 -0800

    :heavy_check_mark: cleanup

--- NetCoreUnitTestProject/Base/NewForNet8.cs
+++ NetCoreUnitTestProject/Base/NewForNet8.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BaseModelsLibrary.Models;
+﻿using BaseModelsLibrary.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 // ReSharper disable once CheckNamespace
--- NetCoreUnitTestProject/NewForNet8.cs
+++ NetCoreUnitTestProject/NewForNet8.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
-using BaseDataValidatorLibrary.Helpers;
-using BaseDataValidatorLibrary.LanguageExtensions;
+﻿using BaseDataValidatorLibrary.Helpers;
 using BaseModelsLibrary.Models;
 using NetCoreUnitTestProject.Base;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NFluent;
-using System.ComponentModel.DataAnnotations;
 
 namespace NetCoreUnitTestProject;
 
@@ -32,35 +21,22 @@ public partial class NewForNet8 : TestBase
         EntityValidationResult validationResult = Model.Validate(_state);
         Check.That(validationResult.HasError).IsFalse();
     }
+    [TestMethod]
+    [TestTraits(Trait.net8Additions)]
+    public void IsNotValidState()
+    {
+        string expectedStateValue = "Invalid state";
+        string expectedPostalValue = "Invalid postal code";
+        State state = _state;
+        state.Name = "California";
+        state.PostalCode = "11111";
+        EntityValidationResult validationResult = Model.Validate(_state);
 
-
-            [TestMethod]
-            [TestTraits(Trait.net8Additions)]
-            public void IsNotValidState()
-            {
-                string expectedStateValue = "Invalid state";
-                string expectedPostalValue = "Invalid postal code";
-                State state = _state;
-                state.Name = "California";
-                state.PostalCode = "11111";
-                EntityValidationResult validationResult = Model.Validate(_state);
-
-                Check.That(validationResult.Errors[0].ErrorMessage)
-                    .Equals(expectedStateValue);
-                Check.That(validationResult.Errors[1].ErrorMessage)
-                    .Equals(expectedPostalValue);
-            }
-
-
-
-
-
-
-
-
-
-
-
+        Check.That(validationResult.Errors[0].ErrorMessage)
+            .Equals(expectedStateValue);
+        Check.That(validationResult.Errors[1].ErrorMessage)
+            .Equals(expectedPostalValue);
+    }
 }
 
 
